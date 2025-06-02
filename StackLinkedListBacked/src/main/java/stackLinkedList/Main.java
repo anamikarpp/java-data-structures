b5fd30cd9db1a2f23cf99a0deca6166dcd9d31ee
Anamika Patel <51049128+anamikarpp@users.noreply.github.com>
2025-06-02 15:20:09 +0530

    Updated stack backed by linkedlist

---
+++ StackLinkedListBacked/src/main/java/stackLinkedList/Main.java
@@ -0,0 +1,24 @@
+package stackLinkedList;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane","Jones",123);
+        Employee johnDoe = new Employee("John","Doe",4567);
+        Employee marySmith = new Employee("Mary","Smith",22);
+        Employee mikeWilson = new Employee("Mike","Wilson",3245);
+
+        LinkedStack stack = new LinkedStack();
+        stack.push(janeJones);
+        stack.push(johnDoe);
+        stack.push(marySmith);
+        stack.push(mikeWilson);
+
+        stack.printStack();
+        System.out.println(stack.peek());
+        stack.pop();
+        stack.printStack();
+
+    }
+}
